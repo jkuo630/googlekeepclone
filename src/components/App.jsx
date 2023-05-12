0cf3e7c53aee1e788d420c498a1e88c9cb0a6257
jkuo630 <89288700+jkuo630@users.noreply.github.com>
2023-05-11 22:51:39 -0400

    finished, pre-styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  //Creating a new state that is an array of notes
+  const [notes, setNotes] = useState([]);
+
+  //Function that adds new notes to the state
+  function addNote(note) {
+    //Arrow function that takes in the previous notes and adds the new one
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
+
+  //Filter functions can take in up to 3 parameters
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  //The map function loops through each note in the array, and then the arrow function is performed on each item
+  //The arrow function turns each item into the standard note format with its corresponding title and content
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  //New state that represents each note
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  //When the form detects a change it runs this function
+  function handleChange(event) {
+    //Deconstructs the event to a name and value
+    const { name, value } = event.target;
+
+    //Set note area as what is being typed
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  //Pushes the note to app.jsx
+  //Prevents the website from refreshing each time a button is clicked
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
